79b5126a0394c048ce8c6af159fc58c43746ee46
ryan-avancena <ravancena.dev@gmail.com>
2024-02-25 04:57:17 -0800

    commit

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,18 +1,21 @@
 from flask import Flask 
+from scrape import scrape
 
 app = Flask(__name__)
-
-sample = 5
-
 
 def indexPage():
     # map_content = getMap()
     return f"""
-    <div id="map" style="height: 80vw; width: 80vh"></div>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-    <script src="static/index.js"></script>
-    <script src="leaflet-heat.js"></script>
+    <body>
+        <div class="header"> FULLYPARKS</div>
+        <div id="map"></div>
+        <div id="info"></div>
+    </body>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+        <link rel="stylesheet" href="./static/index.css" />
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+        <script src="static/index.js"></script>
+        <script src="leaflet-heat.js"></script>
     """
     
 
@@ -20,6 +23,10 @@ def indexPage():
 @app.route("/")
 def main():
     return indexPage()
+
+@app.route("/api")
+def api():
+    return scrape()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- flask-server/static/index.js
+++ flask-server/static/index.js
@@ -1,33 +1,97 @@
-var map = L.map('map').setView([33.88134, -117.8818], 15);
+var map = L.map('map').setView([33.8828022691726, -117.88558213440378], 16);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// var heat = L.heatLayer([
-// 	[50.5, 30.5, 0.2], // lat, lng, intensity
-// 	[50.6, 30.4, 0.5],
-// ], {radius: 25}).addTo(map);
 
-// var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-// 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-// 	subdomains: 'abcd',
-// 	maxZoom: 20
-// });
-var polygonCoords = [
-    [33.88005, -117.8823], // top left
-    [33.8808, -117.8823], // top right
-    [33.8808, -117.8812], // bottom right
-    [33.88005, -117.8812] // bottom left
-];
+const lowColor = {
+    red: 255,
+    green: 49,
+    blue: 49
+};
+const mediumColor = {
+    red: 255,
+    green: 255,
+    blue: 51
+};
+const highColor = {
+    red: 57,
+    green: 255,
+    blue: 20
+};
 
-var polygon = L.polygon(polygonCoords, {
-    color: 'red', // Polygon border color
-    fillColor: 'blue', // Polygon fill color
-    fillOpacity: 0.5 // Opacity of the fill color
-}).addTo(map);
+function colorGradient(fadeFraction, rgbColor1, rgbColor2, rgbColor3) {
+    var color1 = rgbColor1;
+    var color2 = rgbColor2;
+    var fade = fadeFraction;
 
-polygon._path.setAttribute("style", "border-radius: 10px");
+    if (rgbColor3) {
+      fade = fade * 2;
+
+      if (fade >= 1) {
+        fade -= 1;
+        color1 = rgbColor2;
+        color2 = rgbColor3;
+      }
+    }
+
+    var diffRed = color2.red - color1.red;
+    var diffGreen = color2.green - color1.green;
+    var diffBlue = color2.blue - color1.blue;
+
+    var gradient = {
+      red: parseInt(Math.floor(color1.red + (diffRed * fade)), 10),
+      green: parseInt(Math.floor(color1.green + (diffGreen * fade)), 10),
+      blue: parseInt(Math.floor(color1.blue + (diffBlue * fade)), 10),
+    };
+
+    return 'rgb(' + gradient.red + ',' + gradient.green + ',' + gradient.blue + ')';
+  }
+  let dicAreas;
+
+  fetch('/api')
+      .then(response => response.json())
+      .then(areas => {
+          dicAreas = areas;
+          getGradient(dicAreas);
+          console.log(dicAreas)
+      })
+      .catch(error => {
+          // Handle errors
+          console.error('Error fetching data:', error);
+      });
+  
+    var circles = {
+        "Nutwood Structure" : { center: [33.87900170185511, -117.88865432488892], radius: 150, tooltip: "Nutwood Parking" },
+        "Eastside Structure" : { center: [33.88053842742955, -117.88163836042212], radius: 150, tooltip: "Eastside Parking"},
+        "State College Structure" : { center: [33.88189, -117.88927], radius: 150, tooltip: "State College Parking" },
+        "Lot A & G" : { center: [33.8873, -117.88846], radius: 150, tooltip: "Lot A & G" }
+    };
+
+
+    function getGradient(dicAreas) {
+        console.log(dicAreas)
+        for (let key in circles) {
+
+            console.log(key, circles)
+            const center = circles[key].center;
+            const fade_percentage = dicAreas[key].fade_percentage;
+            const color = colorGradient(fade_percentage, lowColor, mediumColor, highColor);
+            
+            var circle = L.circle(center, { color: color, radius: 150 });
+            circle.bindTooltip("<span class='text-info'>" + circles[key].tooltip + "</span>", {
+                permanent: true,
+                direction: 'center',
+                offset: [0, 0],
+                className: 'transparent-tooltip'
+            });            
+            circle.addTo(map);
+
+            
+            // addTo(map);
+        }
+    }
 
 
 var Jawg_Matrix = L.tileLayer('https://tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
@@ -37,4 +101,8 @@ var Jawg_Matrix = L.tileLayer('https://tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.p
 	accessToken: 'a6J0dqksQhPs7oQY9rohxRxO5YcWaRRalE73ExUez1i7VIottlDMpfienle5A9cS'
 });
 Jawg_Matrix.addTo(map)
+
+
+
+var info = dicAreas
 // CartoDB_DarkMatter.addTo(map)
