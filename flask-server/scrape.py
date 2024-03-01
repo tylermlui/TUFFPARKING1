c2a815413028688f5cd8ad9c7f8b8214737b89c4
Tyler Lui <tylerlui@gmail.com>
2024-02-29 17:10:02 -0800

    FULLERTON CHURCH PARKING FIX

--- flask-server/scrape.py
+++ flask-server/scrape.py
@@ -37,13 +37,21 @@ def scrape():
         left_cols = cols[0].find_all("p")
         right_cols = cols[1]
         availability_str = right_cols.find_all('span')[0].text
-        availability = None if availability_str == "Closed" else int(availability_str)
+        if availability_str == "Closed":
+            availability = None 
+        elif availability_str == "Open":
+            availability = "Open"
+        else:
+            availability = int(availability_str)
+        
         name = left_cols[0].text.strip()
         total_spaces = int(left_cols[1].text.strip().replace("Total Spaces: \n", ""))
         last_update = left_cols[2].text
-        fade_percentage = 0 if availability_str == "Closed" else max(0,min(1, availability/ total_spaces))
+        try:
+            fade_percentage = 0 if availability == None else max(0,min(1, availability/ total_spaces))
+        except: 
+            availability = "Open"
         avg_time = getAverageTime(name)
-
         areas[name] = {
             "availability": availability
             , "total_spaces": total_spaces
